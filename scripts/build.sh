50886a1feb74c7a336b80f3d2927111f727e9013
Oleg Nechaev <lega911@gmail.com>
2015-02-22 02:24:28 +0500

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,7 @@ coffee -o ./tmp/ -c ./alight/compile.coffee
 coffee -o ./tmp/ -c ./alight/directives.coffee
 coffee -o ./tmp/ -c ./alight/compile.coffee
 coffee -o ./tmp/ -c ./alight/core.coffee
+coffee -o ./tmp/ -c ./alight/observer.coffee
 coffee -o ./tmp/ -c ./alight/utilits.coffee
 
 coffee -c ./test/test_drepeat.coffee
@@ -18,8 +19,26 @@ coffee -c ./test/test_scan.coffee
 coffee -c ./test/test_pars.coffee
 coffee -c ./test/test_utilits.coffee
 coffee -c ./test/test_core.coffee
+coffee -c ./test/test_observer.coffee
+coffee -c ./test/test_ns.coffee
+coffee -c ./test/test_compile.coffee
+coffee -c ./test/test_filters.coffee
 
-./scripts/assemble.sh
+# assemble
+cat ./alight/prefix.js > ./bin/alight.js
+cat ./alight/fquery.js >> ./bin/alight.js
+cat ./tmp/core.js >> ./bin/alight.js
+cat ./tmp/utilits.js >> ./bin/alight.js
+cat ./tmp/parser.js >> ./bin/alight.js
+cat ./tmp/compile.js >> ./bin/alight.js
+cat ./tmp/directives.js >> ./bin/alight.js
+cat ./tmp/drepeat.js >> ./bin/alight.js
+cat ./tmp/filters.js >> ./bin/alight.js
+cat ./tmp/observer.js >> ./bin/alight.js
+cat ./alight/postfix.js >> ./bin/alight.js
+
+cp ./tmp/animate.js ./bin/animate.js
+
 
 echo "/**\n * Angular Light\n * (c) 2015 Oleg Nechaev\n * Released under the MIT License.\n */" > ./bin/alight.min.js
 minify ./bin/alight.js >> ./bin/alight.min.js
---
+++ test/phantom.js
@@ -0,0 +1,15 @@
+var page = require('webpage').create();
+var url = './test/run.html';
+
+page.onConsoleMessage = function(msg) {
+	console.log(': ' + msg);
+};
+
+page.open(url, function (status) {
+	console.log('Status', status);
+	//Page is loaded!
+	setTimeout(function(){
+		console.log('run test in a browser is a better way');
+		phantom.exit();
+	}, 7000);
+});
