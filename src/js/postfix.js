2fc1474746accb0ef4ef4f24017b8def81007528
Oleg Nechaev <lega911@gmail.com>
2016-06-08 22:44:40 +0500

    fix postfix

--- src/js/postfix.js
+++ src/js/postfix.js
@@ -13,8 +13,6 @@
 		});
 	} else if(typeof(module) === 'object' && typeof(module.exports) === 'object') {
 		module.exports = alight
-	} else if(typeof(alightInitCallback) === 'function') {
-		alightInitCallback(alight)
 	} else {
 		alight.d.al.ctrl.global = true;  // global controllers
 		window.alight = alight;
