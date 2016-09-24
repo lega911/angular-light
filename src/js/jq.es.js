285c88f485cd2b2e3a0a01fa3de65486f63173d2
Oleg Nechaev <lega911@gmail.com>
2016-09-25 00:02:27 +0200

    return result from jquery plugin

--- src/js/jq.es.js
+++ src/js/jq.es.js
@@ -2,6 +2,6 @@ if(window.jQuery) {
     window.jQuery.fn.alight = function(data) {
         let elements = [];
         this.each((i, el) => elements.push(el));
-        if(elements.length) alight(elements, data);
+        if(elements.length) return alight(elements, data);
     }
 }
